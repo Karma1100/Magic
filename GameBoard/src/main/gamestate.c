5d39ea0bb4e656a027c1e981cbd90edf2d6ca5c8
1100Karma <ajdefafa@gmail.com>
2025-12-11 11:47:23 -0500

    game board

---
+++ GameBoard/src/main/gamestate.c
@@ -0,0 +1,19 @@
+/**
+ *things need to keep tack of:
+ 	Cards in different hands
+	Cards in graveyards/exiled
+	Game state and stack
+ *
+ */
+
+
+
+
+enum GAME_PHASE{
+	BEGINING_PHASE,
+	MAIN_PHASE,
+	COMBAT_PHASE,
+	MAIN_PHASE_TWO,
+	END_PHASE
+}
+
